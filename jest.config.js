5be39e0f97bd711a1281dd34871688a0e272d7cb
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-04-26 22:30:06 -0300

    chore: setup reflect-metadata for jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   roots: ["<rootDir>/src", "<rootDir>/tests"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**", "!<rootDir>/src/**/index.ts"],
   coverageDirectory: "coverage",
   coverageProvider: "babel",
   testMatch: ["**/*.spec.ts"],
   testEnvironment: "node",
   clearMocks: true,
+  setupFilesAfterEnv: ["./jest.setup.ts"],
   moduleNameMapper: {
     "@domain/(.*)": "<rootDir>/src/domain/$1",
     "@infra/(.*)": "<rootDir>/src/infra/$1",
