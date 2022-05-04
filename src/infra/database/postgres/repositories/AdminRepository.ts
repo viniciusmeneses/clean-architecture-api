4337fa704f673fa4e9fda73deb92dac1d60ca0cc
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-04 20:46:13 -0300

    feat: add findByEmail to AdminRepository

--- src/infra/database/postgres/repositories/AdminRepository.ts
+++ src/infra/database/postgres/repositories/AdminRepository.ts
@@ -1,13 +1,19 @@
 import { Admin } from "@domain/entities/Admin";
 import { CreateAdminRepository } from "@domain/ports/repositories/admin/CreateAdminRepository";
+import { FindAdminByEmailRepository } from "@domain/ports/repositories/admin/FindAdminByEmailRepository";
 
 import { Repository } from "../Repository";
 import { AdminSchema } from "../schemas/AdminSchema";
 
-export class AdminRepository extends Repository implements CreateAdminRepository {
+export class AdminRepository extends Repository implements CreateAdminRepository, FindAdminByEmailRepository {
   public async create({ email, password }: CreateAdminRepository.Params): Promise<Admin> {
     const repository = this.connection.getRepository(AdminSchema);
     const admin = repository.create({ email, password });
     return repository.save(admin);
+  }
+
+  public findByEmail(email: string): Promise<Admin> {
+    const repository = this.connection.getRepository(AdminSchema);
+    return repository.findOneBy({ email });
   }
 }
