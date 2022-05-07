7563cb53453eb5ee43707d4ef087e8a15b99d14b
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-07 14:05:56 -0300

    feat: add helper class to respond as http

---
+++ src/presentation/helpers/HttpResponse.ts
@@ -0,0 +1,26 @@
+import { Http } from "@presentation/protocols";
+
+export class HttpResponse {
+  public static created(data: any): Http.Response {
+    return {
+      status: 201,
+      body: data,
+    };
+  }
+
+  public static badRequest(error: Error | Error[]): Http.Response {
+    const errors = error instanceof Error ? [error] : error;
+
+    return {
+      status: 400,
+      body: { errors: errors.map(({ name, message }) => ({ type: name, message })) },
+    };
+  }
+
+  public static serverError(error: Error): Http.Response {
+    return {
+      status: 500,
+      body: error.stack,
+    };
+  }
+}
