5a23342cfe068a1d2b236d51b1bd6723dcd4aeee
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-11 23:59:11 -0300

    chore: add supertest to create e2e tests

--- jest-e2e.config.js
+++ jest-e2e.config.js
@@ -1,4 +1,5 @@
 const config = require("./jest.config");
 
 config.testMatch = ["**/*.test.ts"];
+config.setupFilesAfterEnv = [...config.setupFilesAfterEnv, "./jest-e2e.setup.ts"];
 module.exports = config;
