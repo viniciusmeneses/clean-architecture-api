ad77e8fd4e6bc099b19d14372536e52f33784c13
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-11 23:59:45 -0300

    fix: remove wrong "]" from logger

--- src/main/config/logger.ts
+++ src/main/config/logger.ts
@@ -2,7 +2,7 @@ import log4js from "log4js";
 
 log4js.configure({
   appenders: {
-    console: { type: "console", layout: { type: "pattern", pattern: "%[[%p] | %d{yyyy/MM/dd hh:mm:ss O}]%] %m" } },
+    console: { type: "console", layout: { type: "pattern", pattern: "%[[%p | %d{yyyy/MM/dd hh:mm:ss O}]%] %m" } },
   },
   categories: { default: { appenders: ["console"], level: "info" } },
 });
